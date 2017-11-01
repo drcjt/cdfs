999b5952691e07400cd95ecd0589e803ad66af98
drcjt <drcjt@outlookc.om>
2017-11-01 22:00:15 +0000

    Add test for data node descriptors Add extra bits to .gitignore

---
+++ Tests/NameNodeTests/DataNodeDescriptorTests.cs
@@ -0,0 +1,27 @@
+﻿using NameNode.Service;
+using NUnit.Framework;
+
+namespace NameNodeTests
+{
+    [TestFixture]
+    class DataNodeDescriptorTests
+    {
+        [Test]
+        public void CopyConstructor_Always_CreatesDataNodeDescriptorWithSameProperties()
+        {
+            // Arrange
+            var dataNode = new DataNodeDescriptor();
+            dataNode.HostName = "testHostName";
+            dataNode.IPAddress = "testIPAddress";
+            dataNode.LastUpdate = 999;
+
+            // Act
+            var dataNodeCopy = new DataNodeDescriptor(dataNode);
+
+            // Assert
+            Assert.AreEqual(dataNode.HostName, dataNodeCopy.HostName);
+            Assert.AreEqual(dataNode.IPAddress, dataNodeCopy.IPAddress);
+            Assert.AreEqual(dataNode.LastUpdate, dataNodeCopy.LastUpdate);
+        }
+    }
+}
