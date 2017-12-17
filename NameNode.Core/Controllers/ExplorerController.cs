d7fcfb3f629a7a876279a8caeb9cc0af05bbdd39
drcjt <drcjt@outlookc.om>
2017-12-17 15:24:03 +0000

    Add explorer to .net core version of name node server

---
+++ NameNode.Core/Controllers/ExplorerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NameNode.Core.FileSystem.Interfaces;
+using NameNode.Core.Models;
+
+namespace NameNode.Core.Controllers
+{
+    public class ExplorerController : Controller
+    {
+        private readonly IFileSystem _fileSystem;
+        public ExplorerController(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new FileStatusModel();
+            model.Files = new List<FileStatus>();
+            var files = _fileSystem.GetListing(Request.Query["path"]);
+            foreach (var file in files)
+            {
+                model.Files.Add(new FileStatus() { Name = file.Name, IsFile = file is IFile, FullPath = file.FullPath });
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ NameNode.Core/Models/FileStatusModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NameNode.Core.Models
+{
+    public class FileStatusModel
+    {
+        public IList<FileStatus> Files { get; set; }
+    }
+
+    public class FileStatus
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+        public bool IsFile { get; set; }
+    }
+}
