808eee78341e2314d5c83a65020ee58fa7987087
drcjt <drcjt@outlookc.om>
2018-01-25 23:10:30 +0000

    Add simple data node integration tests

---
+++ Tests/DataNodeIntegrationTests/DataNodeIntegrationTests.cs
@@ -0,0 +1,27 @@
+﻿using DataNode;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace DataNodeIntegrationTests
+{
+    [TestFixture]
+    class DataNodeIntegrationTests
+    {
+        [Test]
+        public async Task GetIndex_Always_ReturnsEmptyString()
+        {
+            // Arrange
+            var testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            var testClient = testServer.CreateClient();
+
+            // Act
+            var response = await testClient.GetAsync("/Index");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.AreEqual("", responseString);
+        }
+    }
+}
